9cfcf7eec4f89fde7a3a95a353381e16bb1bbecf
BB1464 <oluwafemioyedele908@gmail.com>
2022-01-14 22:32:22 +0100

    added significant asterisk

--- Correlation Matrix.R
+++ Correlation Matrix.R
@@ -8,9 +8,24 @@ library(readxl)
 dat <- read_excel('Copy of Sensory Data for Bayesian Analysis.xlsx')
 
 
+M1 <- cor.mtest(dat[,4:9])
+
+M <- cor(dat[,4:9])
 
 
-
+M2 <- cor.mtest(dat[,4:9],conf.level=.95)
+corrplot(cor(dat[,4:9]),
+         method='square',
+         type = 'lower',
+         p.mat = res1$p,
+         insig = 'label_sig',
+         sig.level = c(.001,.01,.05),
+         pch.cex = 0.8,
+         pch.col = 'yellow',
+         tl.cex = 1,
+         addCoef.col = 'black',
+         tl.pos = 'n',
+         outline = TRUE)
 
 
 corrplot.mixed(corr = M,lower = 'number',

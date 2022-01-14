2380254f498af25b5754b508a5bdf7183dcdcc9f
BB1464 <oluwafemioyedele908@gmail.com>
2022-01-14 21:00:30 +0100

    add correlation matrix

--- Correlation Matrix.R
+++ Correlation Matrix.R
@@ -2,3 +2,19 @@
 # Correlation matrix ------------------------------------------------------
 
 library(corrplot)
+library(tidyverse)
+library(readxl)
+
+dat <- read_excel('Copy of Sensory Data for Bayesian Analysis.xlsx')
+M=cor(dat[,4:9])
+ord=corrMatOrder(M,order='AOE')
+M2=M[ord,ord]
+
+
+corrplot.mixed(M,upper = 'ellipse',lower = 'number')
+
+
+
+
+
+corrplot(M,upper = 'ellipse',lower = 'number')

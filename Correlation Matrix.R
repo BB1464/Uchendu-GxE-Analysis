164b4507dd70565ed4fddd93595c743f869ff771
BB1464 <oluwafemioyedele908@gmail.com>
2022-01-14 21:57:33 +0100

    added more line to the corrplot function

--- Correlation Matrix.R
+++ Correlation Matrix.R
@@ -6,15 +6,18 @@ library(tidyverse)
 library(readxl)
 
 dat <- read_excel('Copy of Sensory Data for Bayesian Analysis.xlsx')
-M=cor(dat[,4:9])
-ord=corrMatOrder(M,order='AOE')
-M2=M[ord,ord]
-
-
-corrplot.mixed(M,upper = 'ellipse',lower = 'number')
 
 
 
 
 
-corrplot(M,upper = 'ellipse',lower = 'number')
+
+
+corrplot.mixed(corr = M,lower = 'number',
+               upper = 'ellipse',
+               tl.pos = 'lt',
+               diag = 'u',
+               lower.col = 'black')
+
+
+

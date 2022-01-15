7db7687862a733b866d4e4c9f16dd17d4b2f088b
BB1464 <oluwafemioyedele908@gmail.com>
2022-01-15 12:56:11 +0100

    add significant asterisk line

--- Correlation Matrix.R
+++ Correlation Matrix.R
@@ -42,4 +42,6 @@ corrplot.mixed(corr = M,lower = 'number',
                upper = 'ellipse',
                tl.pos = 'lt',
                diag = 'u',
-               lower.col = 'black', sig.level = c(0.001, 0.01, 0.05),p.mat=M1$p)
+               lower.col = 'black', 
+               sig.level = c(0.001, 0.01, 0.05)
+               ,p.mat=M1$p,pch.cex=0.6)
